0c8b10963bf982d4076dc2c929e650f867b66cc0
michanIDE <init.complete@gmail.com>
2024-09-16 02:09:21 +0900

    finally working fusion neutron activator

--- src/main/java/net/michanide/mekanismneutronactivator/common/tile/machine/TileEntityFusionNeutronActivator.java
+++ src/main/java/net/michanide/mekanismneutronactivator/common/tile/machine/TileEntityFusionNeutronActivator.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
+
 import mekanism.api.AutomationType;
 import mekanism.api.IContentsListener;
 import mekanism.api.RelativeSide;
@@ -27,7 +28,6 @@ import mekanism.common.capabilities.holder.chemical.ChemicalTankHelper;
 import mekanism.common.capabilities.holder.chemical.IChemicalTankHolder;
 import mekanism.common.capabilities.holder.slot.IInventorySlotHolder;
 import mekanism.common.capabilities.holder.slot.InventorySlotHelper;
-import mekanism.common.config.MekanismConfig;
 import mekanism.common.integration.computer.SpecialComputerMethodWrapper.ComputerChemicalTankWrapper;
 import mekanism.common.integration.computer.SpecialComputerMethodWrapper.ComputerIInventorySlotWrapper;
 import mekanism.common.integration.computer.annotation.SyntheticComputerMethod;
@@ -47,11 +47,13 @@ import mekanism.common.tile.interfaces.IBoundingBlock;
 import mekanism.common.tile.prefab.TileEntityRecipeMachine;
 import mekanism.common.util.MekanismUtils;
 import mekanism.common.util.WorldUtils;
+import mekanism.generators.common.content.fusion.FusionReactorMultiblockData;
+import mekanism.generators.common.tile.fusion.TileEntityFusionReactorController;
+import net.michanide.mekanismneutronactivator.common.config.MNAConfig;
 import net.michanide.mekanismneutronactivator.common.registries.MNABlocks;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.biome.Biome;
-import net.minecraft.world.level.biome.Biome.Precipitation;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.AABB;
 
@@ -69,12 +71,8 @@ public class TileEntityFusionNeutronActivator extends TileEntityRecipeMachine<Ga
     @WrappingComputerMethod(wrapper = ComputerChemicalTankWrapper.class, methodNames = {"getOutput", "getOutputCapacity", "getOutputNeeded", "getOutputFilledPercentage"})
     public IGasTank outputTank;
 
-    @SyntheticComputerMethod(getter = "getPeakProductionRate")
-    private float peakProductionRate;
     @SyntheticComputerMethod(getter = "getProductionRate")
     private float productionRate;
-    private boolean settingsChecked;
-    private boolean needsRainCheck;
 
     private final IOutputHandler<@NonNull GasStack> outputHandler;
     private final IInputHandler<@NonNull GasStack> inputHandler;
@@ -83,6 +81,10 @@ public class TileEntityFusionNeutronActivator extends TileEntityRecipeMachine<Ga
     private GasInventorySlot inputSlot;
     @WrappingComputerMethod(wrapper = ComputerIInventorySlotWrapper.class, methodNames = "getOutputItem")
     private GasInventorySlot outputSlot;
+
+    // private double lastPlasmaTemperature = 300;
+    // private double burnTemperature = 100_000_000;
+    // private double burnRatio = 1;
 
     public TileEntityFusionNeutronActivator(BlockPos pos, BlockState state) {
         super(MNABlocks.FUSION_NEUTRON_ACTIVATOR, pos, state, TRACKED_ERROR_TYPES);
@@ -123,33 +125,9 @@ public class TileEntityFusionNeutronActivator extends TileEntityRecipeMachine<Ga
         return builder.build();
     }
 
-    private void recheckSettings() {
-        Level world = getLevel();
-        if (world == null) {
-            return;
-        }
-        Biome b = world.getBiomeManager().getBiome(getBlockPos()).value();
-        needsRainCheck = b.getPrecipitation() != Precipitation.NONE;
-        // Consider the best temperature to be 0.8; biomes that are higher than that
-        // will suffer an efficiency loss (semiconductors don't like heat); biomes that are cooler
-        // get a boost. We scale the efficiency to around 30% so that it doesn't totally dominate
-        float tempEff = 0.3F * (0.8F - b.getBaseTemperature());
-
-        // Treat rainfall as a proxy for humidity; any humidity works as a drag on overall efficiency.
-        // As with temperature, we scale it so that it doesn't overwhelm production. Note the signedness
-        // on the scaling factor. Also note that we only use rainfall as a proxy if it CAN rain; some dimensions
-        // (like the End) have rainfall set, but can't actually support rain.
-        float humidityEff = needsRainCheck ? -0.3F * b.getDownfall() : 0.0F;
-        peakProductionRate = MekanismConfig.general.maxSolarNeutronActivatorRate.get() * (1.0F + tempEff + humidityEff);
-        settingsChecked = true;
-    }
-
     @Override
     protected void onUpdateServer() {
         super.onUpdateServer();
-        if (!settingsChecked) {
-            recheckSettings();
-        }
         inputSlot.fillTank();
         outputSlot.drainTank();
         productionRate = recalculateProductionRate();
@@ -177,15 +155,22 @@ public class TileEntityFusionNeutronActivator extends TileEntityRecipeMachine<Ga
         if (world == null || !canFunction()) {
             return 0;
         }
-        //Get the brightness of the sun; note that there are some implementations that depend on the base
-        // brightness function which doesn't take into account the fact that rain can't occur in some biomes.
-        float brightness = WorldUtils.getSunBrightness(world, 1.0F);
-        //Production is a function of the peak possible output in this biome and sun's current brightness
-        float production = peakProductionRate * brightness;
-        if (needsRainCheck && (world.isRaining() || world.isThundering())) {
-            production *= 0.2F;
+
+        int injectionRate = 0;
+        BlockEntity aboveEntity = WorldUtils.getTileEntity(getLevel(), this.getBlockPos().above(6));
+        if(aboveEntity != null && aboveEntity instanceof TileEntityFusionReactorController){
+            FusionReactorMultiblockData multiblock = ((TileEntityFusionReactorController)aboveEntity).getMultiblock();
+            if(multiblock.isBurning()){
+                injectionRate = multiblock.getInjectionRate();
+            } else {
+                injectionRate = 0;
+            }
+        } else {
+            injectionRate = 0;
         }
-        return production;
+        productionRate = (float) MNAConfig.general.fusionNeutronActivatorMultiplier.get() * (float) injectionRate;
+        // System.out.println(productionRate);
+        return productionRate;
     }
 
     @Nonnull
